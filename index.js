c3c7a65fe007525af68dac078b964b7f9697d56c
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-07-16 14:54:12 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+add(5, 5);
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+multiply(5, 5);
+
+const divide = (num1, num2) => {
+  num1 / num2;
+  return num1 / num2;
+};
+divide(5, 5);
+const square = (num) => {
+  return num ** 2;
+};
+square(5);
